d269cbd7ea542e0dbd02b600eecec26ec451326c
Jan Dix <jan.dix@and-effect.com>
2023-07-10 21:48:39 +0200

    update generated api code

--- frontend/src/services/models/UserCreate.ts
+++ frontend/src/services/models/UserCreate.ts
@@ -8,4 +8,9 @@ export type UserCreate = {
     is_active?: boolean;
     is_superuser?: boolean;
     is_verified?: boolean;
+    roles?: Array<string>;
+    local_chapter?: string;
+    first_name: string;
+    surname: string;
+    gender?: string;
 };
--- frontend/src/services/models/UserRead.ts
+++ frontend/src/services/models/UserRead.ts
@@ -11,4 +11,9 @@ export type UserRead = {
     is_active?: boolean;
     is_superuser?: boolean;
     is_verified?: boolean;
+    roles?: Array<string>;
+    local_chapter?: string;
+    first_name: string;
+    surname: string;
+    gender?: string;
 };
