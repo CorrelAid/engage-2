68d2b00dfd23b69be9b46bbee1ef71b407cc0713
KonradUdoHannes <konrad.woelms@gmail.com>
2023-07-10 17:17:07 +0200

    fixes related to the current pre-commit setup

--- backend/src/app/auth/roles.py
+++ backend/src/app/auth/roles.py
@@ -1,8 +1,7 @@
 import logging
 
-from fastapi import Depends, HTTPException
-
 from app.auth.users import current_active_user
+from fastapi import Depends, HTTPException
 
 logger = logging.getLogger()
 
@@ -10,10 +9,13 @@ logger = logging.getLogger()
 class RoleChecker:
     """Lightweight Class to check a user permission set against
     the set of allowed roles on an endpoint or router."""
+
     def __init__(self, allowed_roles: set):
         self.allowed_roles = allowed_roles
 
     def __call__(self, user=Depends(current_active_user)):
         if not set(user.roles).isdisjoint(self.allowed_roles):
-            logger.debug(f"User {user.email} with roles {user.roles} not in {self.allowed_roles}")
+            logger.debug(
+                f"User {user.email} with roles {user.roles} not in {self.allowed_roles}"
+            )
             raise HTTPException(status_code=403, detail="Operation not permitted")
--- backend/src/app/database/models.py
+++ backend/src/app/database/models.py
@@ -1,19 +1,21 @@
-from sqlalchemy import Column, ForeignKey, String
-from sqlalchemy.dialects.postgresql import UUID, ARRAY
-from sqlalchemy.orm import DeclarativeBase, relationship
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID
 from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyBaseAccessTokenTableUUID
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.dialects.postgresql import ARRAY, UUID
+from sqlalchemy.orm import DeclarativeBase, relationship
 
 
 class Base(DeclarativeBase):
     pass
+
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
     """
     Basic Userclass used for fast-api users extended with
     Correlaid attribute field to have all user data in one place.
     """
-    roles = Column(ARRAY(String))
+
+    roles = Column(ARRAY(String))  # type: ignore
     local_chapter = Column(String, nullable=True)
     first_name = Column(String)
     surname = Column(String)
@@ -31,7 +33,6 @@ class PartnerOrganization(Base):
     name = Column(String)
     general_contact_id = Column(UUID, ForeignKey("contacts.id"), unique=True)
     general_contact = relationship("Contacts", back_populates="organization")
-    #project_contact = relationship("Contacts")
     local_chapter = Column(String)
     digital_maturity = Column(String, nullable=True)
     data_culture = Column(String, nullable=True)
@@ -46,4 +47,3 @@ class Contacts(Base):
     email = Column(String)
     phone = Column(String, nullable=True)
     organization = relationship("PartnerOrganization")
-    #projects = relationship("")
