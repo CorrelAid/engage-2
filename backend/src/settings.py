d8e0fde66383a98f5ea6ef55807ec22f657c7d31
Jan Dix <jan.dix@and-effect.com>
2023-07-10 21:37:17 +0200

    update env configuration

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -1,4 +1,4 @@
-from pydantic import BaseSettings, Field
+from pydantic import BaseSettings, Field, SecretStr
 
 
 class Settings(BaseSettings):
@@ -7,11 +7,11 @@ class Settings(BaseSettings):
     DATABASE_PORT: int = Field(default=5432)
     DATABASE_NAME: str = Field(default=...)
     DATABASE_USER: str = Field(default=...)
-    DATABASE_PASSWORD: str = Field(default=...)
+    DATABASE_PASSWORD: SecretStr = Field(default=...)
 
     @property
     def database_url(self) -> str:
-        return f"postgresql+asyncpg://{self.DATABASE_USER}:{self.DATABASE_PASSWORD}@{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_NAME}"  # noqa: E501
+        return f"postgresql+asyncpg://{self.DATABASE_USER}:{self.DATABASE_PASSWORD.get_secret_value()}@{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_NAME}"  # noqa: E501
 
 
 settings = Settings()
