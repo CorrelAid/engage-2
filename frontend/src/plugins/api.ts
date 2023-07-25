ce8513b3db0dc1cadfb0f759dca2924632f60c87
Jan Dix <jan.dix@and-effect.com>
2023-07-25 17:51:17 +0200

    introduce api base url

--- frontend/src/plugins/api.ts
+++ frontend/src/plugins/api.ts
@@ -1,6 +1,6 @@
 import { AppClient } from "@/services";
 
 export const apiClient = new AppClient({
-  BASE: "http://localhost:8000",
+  BASE: import.meta.env.VITE_API_BASE_URL,
   WITH_CREDENTIALS: true,
 });
