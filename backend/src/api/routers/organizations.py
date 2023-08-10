1a85273a54fabd2addebb1191bb3e94f8d4f7d35
Jan Dix <jan.dix@and-effect.com>
2023-08-10 19:28:51 +0200

    update return type annotations and remove response_model argument

--- backend/src/api/routers/organizations.py
+++ backend/src/api/routers/organizations.py
@@ -1,21 +1,22 @@
 from copy import copy
 from datetime import datetime
-from typing import Annotated, Any, Literal, Sequence
+from typing import Annotated, Any, Literal
 from uuid import UUID, uuid4
 
 from api.auth.users import current_active_user
-from database.session import get_async_session
-from fastapi import APIRouter, Depends, HTTPException, Path
+from database.session import get_async_session, transactional_session
+from fastapi import APIRouter, Body, Depends, HTTPException, Path
 from models import Organization, OrganizationContact
 from models.user import User
-from pydantic import BaseModel, ConfigDict, model_validator
-from sqlalchemy import delete, insert, select
+from pydantic import BaseModel, ConfigDict, TypeAdapter, model_validator
+from sqlalchemy import select
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
 router = APIRouter(prefix="/organizations", tags=["organizations"])
 
-LEGAL_FORMS = Literal[
+LEGAL_FORM = Literal[
     "e.V. - Eingetragener Verein",
     "gGmbH - Gemeinnützige Gesellschaft mit beschränkter Haftung",
     "GmbH - Gesellschaft mit beschränkter Haftung",
@@ -24,23 +25,13 @@ LEGAL_FORMS = Literal[
     "Stiftung",
 ]
 
-SECTORS = Literal[
+SECTOR = Literal[
     "Bildung",
     "Gesundheit",
     "Kultur",
     "Sport",
     "Umwelt",
 ]
-
-
-async def get_organization_by_id(
-    db_session: Annotated[AsyncSession, Depends(get_async_session)],
-    organization_id: Annotated[UUID, Path()],
-) -> Organization:
-    organization = await db_session.get(Organization, organization_id)
-    if organization is None:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, "Organization not found.")
-    return organization
 
 
 class OrganizationContactRead(BaseModel):
@@ -57,8 +48,8 @@ class OrganizationRead(BaseModel):
 
     id: UUID
     name: str
-    legal_form: LEGAL_FORMS
-    sectors: list[SECTORS]
+    legal_form: LEGAL_FORM
+    sectors: list[SECTOR]
     contacts: list[OrganizationContactRead]
     created_at: datetime
     created_by: str
@@ -79,100 +70,173 @@ class OrganizationRead(BaseModel):
 
 class OrganizationCreate(BaseModel):
     name: str
-    legal_form: LEGAL_FORMS
-    sectors: list[SECTORS]
+    legal_form: LEGAL_FORM
+    sectors: list[SECTOR]
     contacts: list[OrganizationContactRead] = []
-
-
-@router.post(path="", response_model=OrganizationRead)
-async def create_organization(
-    organization: OrganizationCreate,
-    current_active_user: Annotated[User, Depends(current_active_user)],
-    db_session: Annotated[AsyncSession, Depends(get_async_session)],
-) -> Organization:
-    new_organization = Organization(
-        id=uuid4(),
-        name=organization.name,
-        legal_form=organization.legal_form,
-        created_by=current_active_user.id,
-        updated_by=current_active_user.id,
-        sectors=organization.sectors,
-        contacts=[
-            OrganizationContact(**contact.model_dump())
-            for contact in organization.contacts
-        ],
-    )
-    db_session.add(instance=new_organization)
-    await db_session.commit()
-    return new_organization
-
-
-@router.get(path="", response_model=list[OrganizationRead])
-async def list_organizations(
-    db_session: Annotated[AsyncSession, Depends(get_async_session)],
-) -> Sequence[Organization]:
-    result = await db_session.execute(select(Organization))
-    return result.scalars().all()
-
-
-@router.get(path="/{organization_id}", response_model=OrganizationRead)
-async def get_organization(
-    organization: Annotated[Organization, Depends(get_organization_by_id)],
-) -> Organization:
-    return organization
 
 
 class OrganizationUpdate(BaseModel):
     name: str
-    legal_form: LEGAL_FORMS
-    sectors: list[SECTORS]
+    legal_form: LEGAL_FORM
+    sectors: list[SECTOR]
     contacts: list[OrganizationContactRead] = []
 
 
-@router.patch(path="/{organization_id}", response_model=OrganizationRead)
-async def update_organization(
-    organization: Annotated[Organization, Depends(get_organization_by_id)],
-    updated_organization: OrganizationUpdate,
-    current_active_user: Annotated[User, Depends(current_active_user)],
-    db_session: Annotated[AsyncSession, Depends(get_async_session)],
-) -> Organization:
-    for key, value in updated_organization.model_dump(
-        exclude_defaults=True, exclude={"contacts": True}
-    ).items():
-        if getattr(organization, key) != value:
-            setattr(organization, key, value)
-    organization.updated_by = current_active_user.id
-    delete_query = delete(table=OrganizationContact).where(
-        OrganizationContact.organization_id == organization.id
-    )
-    await db_session.execute(statement=delete_query)
-    if updated_organization.contacts:
-        insert_query = insert(table=OrganizationContact)
-        await db_session.execute(
-            statement=insert_query,
-            params=[
-                {"organization_id": organization.id, **contact.model_dump()}
-                for contact in updated_organization.contacts
+class OrganizationStore:
+    def __init__(
+        self,
+        session: Annotated[AsyncSession, Depends(transactional_session)],
+    ) -> None:
+        self.session = session
+
+    async def _get_orm_organization(self, organization_id: UUID) -> Organization:
+        try:
+            stmt = select(Organization).where(Organization.id == organization_id)
+            organization_entry = (await self.session.execute(stmt)).scalar_one()
+        except NoResultFound:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        return organization_entry
+
+    async def get(self, organization_id: UUID) -> OrganizationRead:
+        organization_entry = await self._get_orm_organization(organization_id)
+        return TypeAdapter(OrganizationRead).validate_python(organization_entry)
+
+    async def get_all(self) -> list[OrganizationRead]:
+        organization_entries = (
+            (await self.session.execute(select(Organization))).scalars().all()
+        )
+        return TypeAdapter(list[OrganizationRead]).validate_python(organization_entries)
+
+    async def create(
+        self, organization: OrganizationCreate, creator_id: UUID
+    ) -> OrganizationRead:
+        new_organization = Organization(
+            id=uuid4(),
+            **organization.model_dump(exclude={"contacts"}),
+            contacts=[
+                OrganizationContact(**contact.model_dump())
+                for contact in organization.contacts
             ],
         )
-    return organization
+        self.session.add(new_organization)
+        return TypeAdapter(OrganizationRead).validate_python(new_organization)
+
+    async def delete(self, organization_id: UUID) -> None:
+        organization_entry = await self._get_orm_organization(organization_id)
+        await self.session.delete(organization_entry)
+        return None
+
+    async def update(
+        self,
+        organization_id: UUID,
+        organization: OrganizationUpdate,
+        updater_id: UUID,
+    ) -> OrganizationRead:
+        organization_entry = await self._get_orm_organization(organization_id)
+        for key, value in organization.model_dump(exclude_defaults=True).items():
+            if getattr(organization_entry, key) != value:
+                setattr(organization_entry, key, value)
+        organization_entry.updated_by = updater_id
+        organization_entry.updated_at = datetime.utcnow()
+        return TypeAdapter(OrganizationRead).validate_python(organization_entry)
+
+    async def archive(
+        self, organization_id: UUID, archiver_id: UUID
+    ) -> OrganizationRead:
+        organization_entry = await self._get_orm_organization(organization_id)
+        organization_entry.archived_by = archiver_id
+        organization_entry.archived_at = datetime.utcnow()
+        return TypeAdapter(OrganizationRead).validate_python(organization_entry)
+
+    async def unarchive(self, organization_id: UUID) -> OrganizationRead:
+        organization_entry = await self._get_orm_organization(organization_id)
+        organization_entry.archived_by = None
+        organization_entry.archived_at = None
+        return TypeAdapter(OrganizationRead).validate_python(organization_entry)
 
 
-@router.patch(path="/{organization_id}/archive", response_model=OrganizationRead)
-async def archive_organization(
-    db_session: Annotated[AsyncSession, Depends(get_async_session)],
-    organization: Annotated[Organization, Depends(get_organization_by_id)],
-    current_active_user: Annotated[User, Depends(current_active_user)],
-) -> Organization:
-    organization.toggle_archive(archived_by=current_active_user.id)
-    await db_session.commit()
-    await db_session.refresh(organization)
-    return organization
+@router.get(
+    path="/",
+    response_description="List of organizations",
+    dependencies=[Depends(current_active_user)],
+)
+async def list_organizations(
+    project_store: Annotated[OrganizationStore, Depends()],
+) -> list[OrganizationRead]:
+    return await project_store.get_all()
 
 
-@router.delete(path="/{organization_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.post(
+    path="/",
+    response_description="Create a new organization",
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_organization(
+    organization: Annotated[OrganizationCreate, Body(...)],
+    project_store: Annotated[OrganizationStore, Depends(get_async_session)],
+    creator: User = Depends(current_active_user),
+) -> OrganizationRead:
+    return await project_store.create(organization, creator_id=creator.id)
+
+
+@router.get(
+    path="/{organization_id}",
+    response_description="Get a single organization",
+    dependencies=[Depends(current_active_user)],
+)
+async def get_organization(
+    organization_id: Annotated[UUID, Path(...)],
+    project_store: Annotated[OrganizationStore, Depends()],
+) -> OrganizationRead:
+    return await project_store.get(organization_id)
+
+
+@router.delete(
+    path="/{organization_id}",
+    response_description="Delete a single organization",
+    dependencies=[Depends(current_active_user)],
+    status_code=status.HTTP_204_NO_CONTENT,
+)
 async def delete_organization(
-    db_session: Annotated[AsyncSession, Depends(get_async_session)],
-    organization: Annotated[Organization, Depends(get_organization_by_id)],
+    organization_id: Annotated[UUID, Path(...)],
+    project_store: Annotated[OrganizationStore, Depends(get_async_session)],
 ) -> None:
-    await db_session.delete(organization)
+    return await project_store.delete(organization_id)
+
+
+@router.patch(
+    path="/{organization_id}",
+    response_description="Update a single organization",
+)
+async def update_organization(
+    organization_id: Annotated[UUID, Path(...)],
+    organization: Annotated[OrganizationUpdate, Body(...)],
+    project_store: Annotated[OrganizationStore, Depends(get_async_session)],
+    updater: User = Depends(current_active_user),
+) -> OrganizationRead:
+    return await project_store.update(
+        organization_id, organization, updater_id=updater.id
+    )
+
+
+@router.patch(
+    path="/{organization_id}/archive",
+    response_description="Archive a single organization",
+)
+async def archive_organization(
+    organization_id: Annotated[UUID, Path(...)],
+    project_store: Annotated[OrganizationStore, Depends(get_async_session)],
+    archiver: User = Depends(current_active_user),
+) -> OrganizationRead:
+    return await project_store.archive(organization_id, archiver_id=archiver.id)
+
+
+@router.patch(
+    path="/{organization_id}/unarchive",
+    response_description="Unarchive a single organization",
+)
+async def unarchive_organization(
+    organization_id: Annotated[UUID, Path(...)],
+    project_store: Annotated[OrganizationStore, Depends(get_async_session)],
+) -> OrganizationRead:
+    return await project_store.unarchive(organization_id)
